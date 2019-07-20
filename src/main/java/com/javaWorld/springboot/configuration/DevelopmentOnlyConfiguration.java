3d438660850dead49c46ec6e04845c2a61e26302
Chinnapa Reddy <chinnapa1986.java@gmail.com>
2019-07-20 08:42:30 -0500

    Springboot application with rest services

---
+++ src/main/java/com/javaWorld/springboot/configuration/DevelopmentOnlyConfiguration.java
@@ -0,0 +1,12 @@
+package com.javaWorld.springboot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+public class DevelopmentOnlyConfiguration {
+	@Bean
+	public String dummy() {
+		return "something";
+	}
+}
